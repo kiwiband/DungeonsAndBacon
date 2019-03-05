279c7d09e6c8f5c8fcdda2c2734c19f21b77a046
dkaznacheev <dmitry.kaznacheev@gmail.com>
2019-03-06 01:39:16 +0300

    Centered movement around the player

--- src/main/kotlin/kiwiband/dnb/Game.kt
+++ src/main/kotlin/kiwiband/dnb/Game.kt
@@ -1,11 +1,16 @@
 package kiwiband.dnb
 
+import kiwiband.dnb.actors.MapActor
 import kiwiband.dnb.actors.creatures.Player
 import kiwiband.dnb.events.EventTick
 import kiwiband.dnb.map.GlobalMap
 import kiwiband.dnb.map.LocalMap
 import kiwiband.dnb.math.Vec2M
 import kiwiband.dnb.ui.TerminalApp
+
+class Wall: MapActor() {
+    override fun getViewAppearance() = '#'
+}
 
 class Game {
     var tickTime = 0
@@ -16,15 +21,27 @@ class Game {
     private val app = TerminalApp(map, inputManager)
     private val player = Player(map)
 
+    fun addWall(x: Int, y: Int) {
+        val wall = Wall()
+        wall.position.set(x, y)
+        map.actors.add(wall)
+    }
+
+    fun addSomeWalls() {
+        val walls = listOf(
+            3 to 3, 3 to 4, 3 to 5, 3 to 6,
+            8 to 3, 8 to 4, 8 to 5, 8 to 6,
+            4 to 7, 5 to 7, 6 to 7, 7 to 7)
+        walls.forEach { addWall(it.first, it.second) }
+    }
+
     init {
         // think about delete handler from dispatcher
         EventTick.dispatcher.addHandler { onTick() }
         player.position.set(Vec2M(5,5))
         map.actors.add(player)
 
-//        val p = Player(map)
-//        p.position.set(7, 7)
-//        map.actors.add(p)
+        addSomeWalls()
     }
 
     private fun onTick() {
--- src/main/kotlin/kiwiband/dnb/ui/views/MapView.kt
+++ src/main/kotlin/kiwiband/dnb/ui/views/MapView.kt
@@ -1,21 +1,29 @@
 package kiwiband.dnb.ui.views
 
+import kiwiband.dnb.actors.creatures.Player
 import kiwiband.dnb.map.LocalMap
-import kiwiband.dnb.ui.MapDrawUtil.writeText
+import kiwiband.dnb.math.Vec2M
 
 class MapView(private val map: LocalMap, width: Int, height: Int) : View(width, height) {
 
     override fun to2DArray(): Array<CharArray> {
         val result = Array(height) { CharArray(width) { ' ' } }
 
+        val playerPosition = findPlayerPosition()
+        val offsetX = playerPosition.x
+        val offsetY = playerPosition.y
+
         map.actors.forEach {
-            val x = it.position.x
-            val y = it.position.y
-            if (x < width && y < height)
+            val x = it.position.x - offsetX + width / 2
+            val y = it.position.y - offsetY + height / 2
+            if (x >= 0 && y >= 0 && x < width && y < height)
                 result[y][x] = it.getViewAppearance()
         }
 
-        writeText(result, "THIS IS A MAP", 18, 10)
         return result
+    }
+
+    private fun findPlayerPosition(): Vec2M {
+        return map.actors.find { it is Player }?.position ?: Vec2M(width / 2, height / 2)
     }
 }
