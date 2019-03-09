30ae95fb1e4c55b4c5a12785820479bd02927664
Kostya Nedikov <konstantin.a.nedikov@gmail.com>
2019-03-09 20:04:12 +0300

    the most important changes ever!

--- src/main/kotlin/kiwiband/dnb/ui/views/InfoView.kt
+++ src/main/kotlin/kiwiband/dnb/ui/views/InfoView.kt
@@ -7,6 +7,6 @@ class InfoView(width: Int, height: Int) : View(width, height) {
     override fun draw(renderer: Renderer) {
         renderer.writeText("DUNGEONS", Vec2M(10, 4))
         renderer.writeText("AND", Vec2M(12, 5))
-        renderer.writeText("BACONS", Vec2M(11, 6))
+        renderer.writeText("BACON", Vec2M(11, 6))
     }
 }
