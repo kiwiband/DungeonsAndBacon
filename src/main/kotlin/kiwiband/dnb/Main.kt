b49d3c0b5dec5534e8ed70e098379f9e8ca73633
Kostya Nedikov <konstantin.a.nedikov@gmail.com>
2019-03-13 14:10:11 +0300

    minor Main class refactoring

--- src/main/kotlin/kiwiband/dnb/Main.kt
+++ src/main/kotlin/kiwiband/dnb/Main.kt
@@ -2,11 +2,9 @@ package kiwiband.dnb
 
 import kiwiband.dnb.ui.App
 
-class Main {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            App().start()
-        }
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        App().start()
     }
 }
