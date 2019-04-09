84c44bdf10d9ec8a0b2ac6360bb3a8bf9da75719
vasyoid <vasyoid@gmail.com>
2019-04-09 20:03:27 +0300

    Add filename parameter to MapSaver methods

--- src/main/kotlin/kiwiband/dnb/map/MapSaver.kt
+++ src/main/kotlin/kiwiband/dnb/map/MapSaver.kt
@@ -3,26 +3,21 @@ package kiwiband.dnb.map
 import org.json.JSONObject
 import java.io.File
 import java.lang.RuntimeException
-import java.nio.file.Files
-import java.nio.file.Paths
 
-object MapSaver {
-    fun saveToFile(map: LocalMap) {
-        val file = File(MAP_FILE)
-        if (!file.exists()) {
-            Files.createDirectory(Paths.get(MAP_FILE).parent)
-        }
-        File(MAP_FILE).writeText(map.toJSON().toString())
+class MapSaver {
+
+    fun saveToFile(map: LocalMap, filename: String) {
+        val file = File(filename)
+        file.parentFile.mkdirs()
+        file.writeText(map.toJSON().toString())
     }
 
-    fun loadFromFile(): LocalMap {
-        if (!checkSaved()) {
+    fun loadFromFile(filename: String): LocalMap {
+        if (!checkSaved(filename)) {
             throw RuntimeException("No saved maps.")
         }
-        return LocalMap.loadMap(JSONObject(File(MAP_FILE).readText()))
+        return LocalMap.loadMap(JSONObject(File(filename).readText()))
     }
 
-    fun checkSaved() = File(MAP_FILE).exists()
-
-    private const val MAP_FILE = "./maps/saved_map.dnd"
+    fun checkSaved(filename: String) = File(filename).exists()
 }
