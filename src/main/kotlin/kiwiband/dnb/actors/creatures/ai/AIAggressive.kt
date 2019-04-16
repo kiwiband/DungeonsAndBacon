42ce8265e63d12ea0e45d86d4a2d156a532f0322
Kostya Nedikov <konstantin.a.nedikov@gmail.com>
2019-04-17 00:23:54 +0300

    fix aggressive AI vec2

--- src/main/kotlin/kiwiband/dnb/actors/creatures/ai/AIAggressive.kt
+++ src/main/kotlin/kiwiband/dnb/actors/creatures/ai/AIAggressive.kt
@@ -2,11 +2,11 @@ package kiwiband.dnb.actors.creatures.ai
 
 import kiwiband.dnb.actors.creatures.Creature
 import kiwiband.dnb.actors.creatures.Player
-import kiwiband.dnb.math.Vec2M
+import kiwiband.dnb.math.Vec2
 
 class AIAggressive(pawn: Creature, private val viewRange: Int) : AIBase(pawn) {
 
-    private var path: MutableList<Vec2M>? = null
+    private var path: MutableList<Vec2>? = null
 
     override fun nextMove() {
         val map = pawn.map
