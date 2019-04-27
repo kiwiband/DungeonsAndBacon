c9e985da1732ea507587086be57a3fc7809db6e1
vasyoid <vasyoid@gmail.com>
2019-04-28 02:43:13 +0300

    Add inventory tests

---
+++ src/test/kotlin/kiwiband/dnb/inventory/InventoryTest.kt
@@ -0,0 +1,39 @@
+package kiwiband.dnb.inventory
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class InventoryTest {
+
+    @Test
+    fun capacityTest() {
+        val inventory = Inventory(2)
+        assertTrue(inventory.add(ArmorItem(1, "", "")))
+        assertTrue(inventory.add(ArmorItem(1, "", "")))
+        assertFalse(inventory.add(ArmorItem(1, "", "")))
+    }
+
+    @Test
+    fun hasSpaceTest() {
+        val inventory = Inventory(2)
+        inventory.add(ArmorItem(1, "", ""))
+        assertTrue(inventory.hasSpace())
+        inventory.add(ArmorItem(1, "", ""))
+        assertFalse(inventory.hasSpace())
+    }
+
+    @Test
+    fun getTest() {
+        val inventory = Inventory(10)
+        val items = listOf<Item>(
+            ArmorItem(1, "asd", "xcv"),
+            WeaponItem(3, "sdf", "qwe"),
+            ArmorItem(1, "asd", "xcv"),
+            WeaponItem(3, "sdf", "qwe")
+        )
+        for (item in items) {
+            inventory.add(item)
+        }
+        assertArrayEquals(items.toTypedArray(), inventory.items().toTypedArray())
+    }
+}
