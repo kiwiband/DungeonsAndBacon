198c68d7e7a629b0a765db7c58dedfad03cded61
vasyoid <vasyoid@gmail.com>
2019-06-05 01:52:28 +0300

    Add client module

--- client/src/test/kotlin/kiwiband/dnb/inventory/InventoryTest.kt
+++ client/src/test/kotlin/kiwiband/dnb/inventory/InventoryTest.kt
@@ -8,17 +8,17 @@ class InventoryTest {
     @Test
     fun capacityTest() {
         val inventory = Inventory(2)
-        assertTrue(inventory.add(ArmorItem(1, "", "")))
-        assertTrue(inventory.add(ArmorItem(1, "", "")))
-        assertFalse(inventory.add(ArmorItem(1, "", "")))
+        assertTrue(inventory.add(ArmorItem(1, 1, "", "")))
+        assertTrue(inventory.add(ArmorItem(1, 1, "", "")))
+        assertFalse(inventory.add(ArmorItem(1, 1, "", "")))
     }
 
     @Test
     fun hasSpaceTest() {
         val inventory = Inventory(2)
-        inventory.add(ArmorItem(1, "", ""))
+        inventory.add(ArmorItem(1, 1, "", ""))
         assertTrue(inventory.hasSpace())
-        inventory.add(ArmorItem(1, "", ""))
+        inventory.add(ArmorItem(1, 1, "", ""))
         assertFalse(inventory.hasSpace())
     }
 
@@ -26,10 +26,10 @@ class InventoryTest {
     fun getTest() {
         val inventory = Inventory(10)
         val items = listOf<Item>(
-            ArmorItem(1, "asd", "xcv"),
-            WeaponItem(3, "sdf", "qwe"),
-            ArmorItem(1, "asd", "xcv"),
-            WeaponItem(3, "sdf", "qwe")
+            ArmorItem(1, 1, "asd", "xcv"),
+            WeaponItem(3, 1, "sdf", "qwe"),
+            ArmorItem(1, 1, "asd", "xcv"),
+            WeaponItem(3, 1, "sdf", "qwe")
         )
         for (item in items) {
             inventory.add(item)
