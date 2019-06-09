5b771b5943479b7733535f815a6e64bda5799832
Kostya Nedikov <konstantin.a.nedikov@gmail.com>
2019-06-09 14:40:38 +0300

    init files for server and client

--- client/src/main/kotlin/kiwiband/dnb/Main.kt
+++ client/src/main/kotlin/kiwiband/dnb/Main.kt
@@ -1,25 +1,33 @@
 package kiwiband.dnb
 
+import org.ini4j.Ini
 import java.io.File
+import java.lang.NumberFormatException
 
 /**
  * Launcher for the client.
  */
 object Main {
-
     private const val DEFAULT_HOST = "localhost"
     private const val DEFAULT_PORT = 12345
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val confFile = File("server.conf")
         var host = DEFAULT_HOST
         var port = DEFAULT_PORT
-        if (confFile.exists()) {
-            confFile.useLines { lins ->
-                val lines = lins.toList()
-                host = lines.elementAt(0)
-                port = lines.elementAt(1).toInt()
+        val settingsFile = File("settings.ini")
+        if (settingsFile.exists()) {
+            val settings = Ini(settingsFile)
+            settings["server"]?.also { serverProfile ->
+                host = serverProfile.getOrDefault("host", DEFAULT_HOST)
+                port = serverProfile.get("port")?.let {
+                    try {
+                        Integer.parseInt(it)
+                    } catch (e: NumberFormatException) {
+                        println("Wrong port format in settings.ini")
+                        DEFAULT_PORT
+                    }
+                } ?: DEFAULT_PORT
             }
         }
         App(host, port).start()
--- server/src/main/kotlin/kiwiband/dnb/server/Main.kt
+++ server/src/main/kotlin/kiwiband/dnb/server/Main.kt
@@ -2,20 +2,38 @@ package kiwiband.dnb.server
 
 import io.grpc.ServerBuilder
 import kiwiband.dnb.events.EventTick
-import kiwiband.dnb.map.LocalMap
+import org.ini4j.Ini
+import java.io.File
+import java.lang.NumberFormatException
 import java.util.concurrent.locks.ReentrantLock
 
 /**
  * Launcher for the server.
  */
 object Main {
+    private val DEFAULT_PORT = 12345;
 
     @JvmStatic
     fun main(args: Array<String>) {
+
+        var port = DEFAULT_PORT
+        val serverFile = File("server.ini")
+        if (serverFile.exists()) {
+            try {
+                port = Integer.parseInt(Ini(serverFile).get("general", "port") ?: DEFAULT_PORT.toString())
+            } catch (e: NumberFormatException) {
+                println("Wrong port format in server.ini")
+            }
+        } else {
+            println("Can't find server.ini")
+        }
+
+        println("Port has been set: $port")
+
         val gameLock = ReentrantLock()
         val gameSession = GameSession()
         val gameService = GameServiceImpl(gameSession, gameLock)
-        ServerBuilder.forPort(12345).addService(gameService).build().start()
+        ServerBuilder.forPort(port).addService(gameService).build().start()
 
         println("Server started")
 
